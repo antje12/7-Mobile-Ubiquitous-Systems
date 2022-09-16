11c577d24d2b903b0d6c6edc96cea4a6d339c1c8
anton <antje12@student.sdu.dk>
2022-09-16 16:37:22 +0200

    fix

--- Mobile-Sensing-Example/base/src/main/java/com/example/android/whileinuselocation/ForegroundOnlyLocationService.kt
+++ Mobile-Sensing-Example/base/src/main/java/com/example/android/whileinuselocation/ForegroundOnlyLocationService.kt
@@ -94,15 +94,15 @@ class ForegroundOnlyLocationService : Service() {
             // IMPORTANT NOTE: Apps running on Android 8.0 and higher devices (regardless of
             // targetSdkVersion) may receive updates less frequently than this interval when the app
             // is no longer in the foreground.
-            interval = TimeUnit.SECONDS.toMillis(10)
+            interval = TimeUnit.SECONDS.toMillis(60)
 
             // Sets the fastest rate for active location updates. This interval is exact, and your
             // application will never receive updates more frequently than this value.
-            fastestInterval = TimeUnit.SECONDS.toMillis(5)
+            fastestInterval = TimeUnit.SECONDS.toMillis(30)
 
             // Sets the maximum time when batched location updates are delivered. Updates may be
             // delivered sooner than this interval.
-            maxWaitTime = TimeUnit.SECONDS.toMillis(30)
+            maxWaitTime = TimeUnit.MINUTES.toMillis(2)
 
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
